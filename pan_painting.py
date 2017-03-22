74eb3dba1c47cd896c1ffe5c53b2bfc7f41d921c
wjb711 <10054053@qq.com>
2017-03-22 21:21:34 +0800

    加入语音功能

--- pan_painting.py
+++ pan_painting.py
@@ -2,12 +2,18 @@
 #我女儿喜欢看小马宝莉，也喜欢给简笔画涂色彩
 #我这个脚本就是给她写的， 随意找张动画片屏幕截图， 就可以制成简笔画了
 #用法python pan_painting.py -i 你的图片 例如 python pan_painting.py -y time.jpg
+#此脚本是windows环境python 2.7+opencv3.2版本的
+#需要提前安装speech模块， 安装方法pip install speech
 import cv2
 #导入cv2这个module模块
 import numpy as np
 #导入numpy这个计算用的模块
 import argparse
 #导入参数模块
+import speech
+#加载语音模块
+speech.say("现在开始")
+#说话， 代表开始
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
@@ -37,9 +43,12 @@ edges_INV =cv2.bitwise_not(edges)
 cv2.imwrite(args["image"],edges_INV)
 #写入源文件， 替换掉原图
 cv2.imshow('2',edges_INV)
+
 #显示新图
 #cv2.imshow('erosion',erosion)
 #cv2.imshow('dilation',dilation)
+speech.say("转换结束")
+#语音提示， 提示程序完成
 cv2.waitKey(0)
 #按任意键退出
 
