21a70cccda2f3f6649ce128295ccd0b7e5218b61
opencv_learner <10054053@qq.com>
2017-03-31 17:12:52 +0800

    1

--- 色彩选择斑点检测.py
+++ 色彩选择斑点检测.py
@@ -19,10 +19,10 @@ cap=cv2.VideoCapture(0)
 while(1):
     ret,im=cap.read()
     #im=cv2.imread('3.jpg')
-    im = cv2.detailEnhance(im, sigma_s=10, sigma_r=0.15)
+    im1 = cv2.detailEnhance(im, sigma_s=10, sigma_r=0.15)
     thrs1 = cv2.getTrackbarPos('色彩选择', '视频色彩')
     print thrs1
-    im_color0 = cv2.applyColorMap(im, thrs1)
+    im_color0 = cv2.applyColorMap(im1, thrs1)
     cv2.imshow("视频色彩", im_color0)
     cv2.imshow("im", im)
     cv2.waitKey(30);
