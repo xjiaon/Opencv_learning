971bb7bd5be7a6da4bf0b2c42d590c112114aed0
wjb711 <10054053@qq.com>
2017-03-31 10:11:41 +0800

    111

---
+++ 色彩选择.py
@@ -0,0 +1,26 @@
+# -*- coding: cp936 -*-
+
+'''
+    OpenCV Colormap  Example
+    
+    Copyright 2015 by Satya Mallick <spmallick@learnopencv.com>
+    
+'''
+
+
+import cv2
+import numpy as np
+def nothing(x):
+    pass
+cv2.namedWindow('视频色彩',0)
+cv2.createTrackbar('色彩选择', '视频色彩', 0, 11, nothing)
+
+cap=cv2.VideoCapture(0)
+while(1):
+    ret,im=cap.read()
+    #im=cv2.imread('3.jpg')
+    thrs1 = cv2.getTrackbarPos('色彩选择', '视频色彩')
+    print thrs1
+    im_color0 = cv2.applyColorMap(im, thrs1)
+    cv2.imshow("视频色彩", im_color0)
+    cv2.waitKey(30);
