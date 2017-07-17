1f111ecafa4093d257ea58b9c13996442263264d
opencv_learner <10054053@qq.com>
2017-07-17 15:50:44 +0800

    1

--- 微信打印照片.py
+++ 微信打印照片.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: cp936 -*-
+# -*- coding: cp936 -*-   #中文注释语句
+#此程序的目的是创建一个webservice网页，网页上可以上传文件， 例如上传照片
+#与此同时， 在文件夹内， 搜索对应后缀的文件，例如jpg, 搜到之后发送到打印机打印， 最后删除照片。
+#也就是说手机访问此网页上传照片， 电脑从后台打印照片
+#也就是类似微信打印机的功能。同理此脚本也可以改造为微信打印word, excel,pdf的程序， 稍加改造即可。
+#如有问题，可以联系本人10054053@qq.com
+#也可以在github上留言
 
-"""Simple HTTP Server With Upload.
-https://github.com/tualatrix/tools/blob/master/SimpleHTTPServerWithUpload.py
-This module builds on BaseHTTPServer by implementing the standard GET
-and HEAD requests in a fairly straightforward manner.
-"""
+
+#注意，本脚本的环境是python2.7，不适用python3， 成功后默认的地址是http://127.0.0.1:8000，对了是windows,具体我的是在win7上跑的。
+
+#需要安装pywin32, 下载位置 https://sourceforge.net/projects/pywin32/
+
+
+
 
 
 import os
@@ -17,9 +24,26 @@ import shutil
 import mimetypes
 import re
 import thread
-import os,time,win32print,sys
+import os,time,win32print,sys,win32ui
+if sys.version_info >= (3,):
+    print ('Error, you should run it in python 2.7, not 3.0+')
+    input("press any key to continue")
+
+#加载各类模块，本脚本大体分两个模块， 一个是http上传， 一个是搜索上传文件并打印。 
+
 class Printer_:
+    hDC = win32ui.CreateDC()
+    
+    printer_name=win32print.GetDefaultPrinter()
+    hDC.CreatePrinterDC (printer_name)
+    #事先要安装i_viewer32这个个软件，并加入系统path
     def printer(self,img):
+        print ('C:/temp/i_view32.exe '+img+' /print')
+        os.system('C:/temp/i_view32.exe '+img+' /print')
+        print ('end')
+         
+    def printer1(self,img): #调用打印机，请事先把默认打印机，纸张格式等参数从电脑上调好
+        print ('start print '+img)
         printer_name = win32print.GetDefaultPrinter ()
     #
     # raw_data could equally be raw PCL/PS read from
@@ -33,14 +57,88 @@ class Printer_:
         hPrinter = win32print.OpenPrinter (printer_name)
         try:
           hJob = win32print.StartDocPrinter (hPrinter, 1, (img, None, "RAW"))
+          print ('here1'+img)
           try:
             win32print.StartPagePrinter (hPrinter)
             win32print.WritePrinter (hPrinter, raw_data)
             win32print.EndPagePrinter (hPrinter)
+            print ('here2')
           finally:
             win32print.EndDocPrinter (hPrinter)
+            print ('here3')
         finally:
           win32print.ClosePrinter (hPrinter)
+          print ('here4')
+    def printer2(self,img):
+        print ('here1'+img)
+        HORZRES = 8
+        VERTRES = 10
+        #
+        # LOGPIXELS = dots per inch
+        #
+        LOGPIXELSX = 88
+        LOGPIXELSY = 90
+        #
+        # PHYSICALWIDTH/HEIGHT = total area
+        #
+        PHYSICALWIDTH = 110
+        PHYSICALHEIGHT = 111
+        #
+        # PHYSICALOFFSETX/Y = left / top margin
+        #
+        PHYSICALOFFSETX = 112
+        PHYSICALOFFSETY = 113
+
+        #printer_name = win32print.GetDefaultPrinter ()
+        print (printer_name)
+        file_name = img
+        print ('here9')
+        print ('here10')
+        #hDC = win32ui.CreateDC()
+        print ('here11')
+        print ('here4')
+        #hDC.CreatePrinterDC (printer_name)
+        print ('here5')
+        printable_area = hDC.GetDeviceCaps (HORZRES), hDC.GetDeviceCaps (VERTRES)
+        print ('here6')
+        printer_size = hDC.GetDeviceCaps (PHYSICALWIDTH), hDC.GetDeviceCaps (PHYSICALHEIGHT)
+        print ('here7')
+        printer_margins = hDC.GetDeviceCaps (PHYSICALOFFSETX), hDC.GetDeviceCaps (PHYSICALOFFSETY)
+        print ('here8')
+
+        #
+        # Open the image, rotate it if it's wider than
+        #  it is high, and work out how much to multiply
+        #  each pixel by to get it as big as possible on
+        #  the page without distorting.
+        #
+        bmp = Image.open (file_name)
+        print ('here3')
+        if bmp.size[0] > bmp.size[1]:
+          bmp = bmp.rotate (90)
+
+        ratios = [1.0 * printable_area[0] / bmp.size[0], 1.0 * printable_area[1] / bmp.size[1]]
+        scale = min (ratios)
+
+        #
+        # Start the print job, and draw the bitmap to
+        #  the printer device at the scaled size.
+        #
+        print ('here2')
+        hDC.StartDoc (file_name)
+        hDC.StartPage ()
+
+        dib = ImageWin.Dib (bmp)
+        scaled_width, scaled_height = [int (scale * i) for i in bmp.size]
+        x1 = int ((printer_size[0] - scaled_width) / 2)
+        y1 = int ((printer_size[1] - scaled_height) / 2)
+        x2 = x1 + scaled_width
+        y2 = y1 + scaled_height
+        dib.draw (hDC.GetHandleOutput (), (x1, y1, x2, y2))
+        print ('here4')
+        hDC.EndPage ()
+        hDC.EndDoc ()
+        hDC.DeleteDC ()
     def img(self,end):
         for i in os.listdir('./'):
             if i.endswith(end):
@@ -48,9 +146,9 @@ class Printer_:
                 #os.system('print')
                 print (i)
                 self.printer(i)
-                time.sleep(10)
+                time.sleep(30)
                 os.remove(i)
-    def __init__(self):
+    def __init__(self): #初始化，并定义搜索后缀，这里默认是jpg
         print ('this is in Printer_')
         while 1>0:
             time.sleep(5)
@@ -58,8 +156,10 @@ class Printer_:
 print ('load module finished')
 def test(a1,a2):
     Printer_()
+##这句很重要， 是建立一个并发程序， 当跑下面的http上传webservice的同时， 并发跑这个打印的模块
 thread.start_new_thread(test,(1,2))
 
+############下面是http上传部分， 这部分是抄别人的, 
 __version__ = "0.1"
 __all__ = ["SimpleHTTPRequestHandler"]
 __author__ = "bones7456"
