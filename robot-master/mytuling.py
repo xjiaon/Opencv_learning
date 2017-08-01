a2039ff682a6c4819c484a8ec4760d02ab9a27e1
opencv_learner <10054053@qq.com>
2017-08-01 22:05:08 +0800

    1
    1

--- robot-master/mytuling.py
+++ robot-master/mytuling.py
@@ -1,12 +1,16 @@
 import urllib.request
 #打开网页用的
+import win32com.client
+#发音用到的
 #import json
 #字符串转字典，这里用到了eval,没用到json,注释掉了
 from urllib.parse import quote
 #中文输入url字符用的， 当url中包含钟文时，需要转换
 #http = urllib3.PoolManager()
-print ("我是智能机器人图灵，想聊点什么？\n")
+print ("我是捷德中国智能机器人图灵大白，想聊点什么？\n")
 #机器人开场白
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+#声音调用这个
 while 1>0:
     
     myword=input('我：\n')
@@ -23,7 +27,13 @@ while 1>0:
         fb=urllib.request.urlopen(url)
         mybytes=fb.read()
         mystr=mybytes.decode('utf8')
-        feedback=eval(mystr)['text']
+        feedback=eval(mystr)['text'].replace("曾先森", "捷德中国")
+        #替换，把曾先森换为捷德中国
     except:
         feedback="没有网络，不跟你好啦"
+    #print (feedback.replace("曾先森", "捷德"))
     print (feedback)
+    #print (type(feedback))
+    
+    speaker.Speak(feedback)
+    #说出话来
