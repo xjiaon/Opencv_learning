601323cbb30850e4e8c06bf6e1e9ca3da682d281
opencv_learner <10054053@qq.com>
2017-08-22 16:36:07 +0800

    1
    1

--- camera.py
+++ camera.py
@@ -1,6 +1,15 @@
 import cv2
+import numpy as np
+def max_cap_size():
+    cap.set(3,3000)
+    cap.set(4,3000)
+    return(int(cap.get(3)),int(cap.get(4)))
 
 cap=cv2.VideoCapture(0)
+wide,high=max_cap_size()
+print (wide,high)
+#cap.set(3,3000)
+#cap.set(4,3000)
 #cap.set(cv2.CAP_PROP_FRAME_WIDTH,1080)
 #cap.set(4,720)
 #cap.set(15, 2.0)
@@ -10,16 +19,20 @@ cap=cv2.VideoCapture(0)
 
 cap.set(10,250)#亮度，最高250
 cap.set(11,250)#对比度，最高250
-cap.set(15,2)#曝光时间，最高0，普通-4
-for i in range(0,37):
-    print (i,cap.get(23))
+cap.set(15 ,2)#曝光时间，最高0，普通-4
+#for i in range(0,37):
+    #print (i,cap.get(23))
 #print (cap.get(3))
 while True:
-    print (cap.get(10),cap.get(11),cap.get(12),cap.get(13),cap.get(14),cap.get(15))
+    #print (cap.get(5),cap.get(10),cap.get(11),cap.get(12),cap.get(13),cap.get(14),cap.get(15))
     _,frame=cap.read()
     gray=cv2.cvtColor(frame,6)
-    cv2.imshow('gray+',gray+gray+gray+gray)
-    cv2.imshow('1',frame)
+    b=np.zeros((high,wide), np.uint8)
+    r=np.zeros((high,wide), np.uint8)
+    #cv2.imshow('gray+',gray+gray)
+    merge=cv2.merge([b,gray,r])
+    #cv2.imshow('1',frame)
+    cv2.imshow('merge',merge)
     cn=cv2.waitKey(1)
     if cn==ord('q'):
         print ('q')
